b23f2249b4e764b51ff121f409ffe7e9cd7bfd01
unknown <pangeran.found@gmail.com>
2021-06-20 19:07:55 +0700

    new validate pembeli

--- routes/pembeli.js
+++ routes/pembeli.js
@@ -10,6 +10,7 @@ const multer = require('multer');
 const { uploadAvatar, uploadRumah, files } = require('../controllers/fileUploadController');
 
 router.get("/hasil", async (req, res) => {
+    if (req.session.user_type == 'pembeli'){
     let pagetitle = "lihat hasil"
     let error = null
     let session = req.session
@@ -17,17 +18,27 @@ router.get("/hasil", async (req, res) => {
     //res.json(result.result.kriteria.length);
     //console.log(JSON.stringify(result.kriteria));
     res.render("../views/pages/pembeli/lihathasil",{pagetitle : pagetitle,error : session.error,name:session.name,session : session,result:result});
+    }else{
+        req.session.error='anda belum login';
+        res.redirect('/dashboard');
+    }
 })
 
 router.get("/akun", async (req, res) => {
+    if (req.session.user_type == 'pembeli'){
     let pagetitle = "akun"
     let error = null
     let session = req.session
     let result = await pembeli.getPembeli(req,res);
     res.render("../views/pages/pembeli/akun",{pagetitle : pagetitle,error : session.error,name:session.name,session : session,result : result});
+}else{
+    req.session.error='anda belum login';
+    res.redirect('/dashboard');
+}
 })
 
 router.post('/upload', function (req, res) {
+    if (req.session.user_type == 'pembeli'){
     files.name = req.session.name;
     uploadAvatar(req, res, async function (err) {
         if (err instanceof multer.MulterError) {
@@ -40,10 +51,19 @@ router.post('/upload', function (req, res) {
         res.redirect('/pembeli/akun');
         // Everything went fine.
     })
+}else{
+    req.session.error='anda belum login';
+    res.redirect('/dashboard');
+}
 })
 
 router.post("/update", async (req, res) => {
+    if (req.session.user_type == 'pembeli'){
     await pembeli.updatePembeli(req, res);
+}else{
+    req.session.error='anda belum login';
+    res.redirect('/dashboard');
+}
 })
 
 
